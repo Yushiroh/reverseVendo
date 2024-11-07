b032fb5f3e26b31586ef7ffd9a495f384b39cbe8
Yushiroh <Yushiroh.13@gmail.com>
2024-11-07 08:13:00 +0800

    files configured and added platformio code for esp32

---
+++ ESPCODE/src/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+
+int but1 = 36;
+int but2 = 39;
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(but1, INPUT);
+  pinMode(but2, INPUT);
+
+}
+
+void loop() {
+
+  int but1State = digitalRead(but1);
+  int but2State = digitalRead(but2);
+
+  if(but1State == 0){
+
+    Serial.println("but1");
+
+  }else if(but2State == 0){
+
+    Serial.println("but2");
+
+  }else{
+    //Serial.println("NO INPUT");
+  }
+
+
+
+}
+
---
+++ reverseVendo.py
@@ -0,0 +1,24 @@
+import torch
+import cv2 as cv
+from ultralytics import YOLO
+
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True:
+ 
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+  
+cap.release()
+cv.destroyAllWindows()
